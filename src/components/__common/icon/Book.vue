9e73378afc85d62d1ecf2f6d0957dc9ef52925a0
Timur Badretdinov <destinerx@gmail.com>
2023-08-06 00:06:32 +0100

    Add icon components

---
+++ src/components/__common/icon/Book.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20"
+    />
+  </svg>
+</template>
