0f7786907553b813624995123dd378f45bb3525c
Timur Badretdinov <destinerx@gmail.com>
2023-08-06 15:09:58 +0100

    Add AlephPage component

---
+++ src/components/_app/AlephPage.vue
@@ -0,0 +1,71 @@
+<template>
+  <main class="page">
+    <nav class="sidebar-toc">
+      <slot name="toc" />
+    </nav>
+    <div class="main">
+      <slot />
+    </div>
+    <nav class="sidebar-outline">
+      <slot name="outline" />
+    </nav>
+    <div class="sidebar-details">
+      <slot name="details" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.page {
+  --header-height: 88px;
+
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - var(--header-height));
+  gap: 0;
+}
+
+@media (min-width: 768px) {
+  .page {
+    flex-direction: row;
+  }
+}
+
+.sidebar-toc {
+  display: flex;
+  flex: 1;
+  padding: 15px 20px 20px;
+  border-right: 1px solid var(--color-border-primary);
+}
+
+@media (min-width: 768px) {
+  .sidebar-toc {
+    min-width: 320px;
+  }
+}
+
+.main {
+  display: flex;
+  flex: 3;
+  padding: 30px;
+}
+
+.sidebar-outline:has(*) {
+  display: flex;
+  flex: 2;
+  padding: 30px 20px 10px;
+  transition: all 0.2s ease-in-out;
+  opacity: 0.5;
+}
+
+.sidebar-outline:has(*):hover {
+  opacity: 1;
+}
+
+.sidebar-details:has(*) {
+  display: flex;
+  flex: 2;
+  padding: 30px 20px 10px;
+  border-right: 1px solid var(--color-border-primary);
+}
+</style>
