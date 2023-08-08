c6c31dd9791e535a60f665807e3d6d00d8ebe036
Timur Badretdinov <destinerx@gmail.com>
2023-08-08 17:17:58 +0100

    Tweak table styling in MarkdownView

--- src/components/__common/MarkdownView.vue
+++ src/components/__common/MarkdownView.vue
@@ -45,4 +45,21 @@ defineProps<{
   border-left: 2px solid hsla(var(--color-accent-hsl) / 30%);
   background-color: hsla(var(--color-accent-hsl) / 2%);
 }
+
+.view :deep(table) {
+  display: block;
+  margin: 1rem 0;
+  overflow-x: auto;
+  border-collapse: collapse;
+}
+
+.view :deep(tr) {
+  border-top: 1px solid var(--color-border-secondary);
+}
+
+.view :deep(td),
+.view :deep(th) {
+  padding: 4px 8px;
+  border: 1px solid var(--color-border-secondary);
+}
 </style>
