7df1688ae85975477cba29a254ccbd16cf231147
Timur Badretdinov <destinerx@gmail.com>
2023-08-10 21:38:27 +0100

    Set up VueDevTools Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import path from 'path';
 
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
