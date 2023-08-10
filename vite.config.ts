880ef265c26188986a41e8c5be09eef50cdc87cf
Timur Badretdinov <destinerx@gmail.com>
2023-08-10 21:34:44 +0100

    Clean up Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,6 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    target: 'es2020',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'es2020',
-    },
-  },
   plugins: [vue()],
   resolve: {
     alias: {
